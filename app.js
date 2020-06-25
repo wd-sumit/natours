8226bc6fcb35bc3d9723edbc367630eb83068ce3
wd-sumit <wdsumit.pal@gmail.com>
2020-06-25 13:32:51 +0530

    Added a comment

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 
+// Start Express App
 const app = express();
-
-
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
